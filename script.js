c92a8bcf7e9ab692cfe98205f6d0c24fa925f80b
DARIO <146894030+restarante007@users.noreply.github.com>
2023-10-10 20:56:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Altera o texto do título
+document.querySelector("h1").textContent = "Programação e Sistemas de
+Informação";
+// Adiciona um link para o GitHub
+document.querySelector("footer").innerHTML += `
+<a
+href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a
+>
+`;
